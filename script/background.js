4a5b87d70d33c4013e9572e9e71f2b90a8316ddd
Mahdyar <hi@mahdyar.me>
2021-03-04 16:33:13 +0330

    feat: add shortcut

--- script/background.js
+++ script/background.js
@@ -1,8 +1,7 @@
-chrome.contextMenus.removeAll();
 chrome.contextMenus.create({
   title: "Send to Telegram",
   documentUrlPatterns: ["<all_urls>"],
-  contexts:["link"],
+  contexts: ["link"],
   onclick: function (link) {
     console.log(link);
     chrome.storage.sync.get(["telemark_code"], function (result) {
@@ -13,4 +12,20 @@ chrome.contextMenus.create({
       }
     });
   },
+});
+chrome.commands.onCommand.addListener(function (command) {
+  if (command == "send-to-telegram") {
+    chrome.storage.sync.get(["telemark_code"], function (result) {
+      if (result.telemark_code) {
+        chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+          let url = tabs[0].url;
+          let title = tabs[0].title;
+          sendToTelegram(url, title, result.telemark_code);
+          alert("Sent!");
+        });
+      } else {
+        alert("First enter your telemark code.");
+      }
+    });
+  }
 });
