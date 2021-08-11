26d4200a4c71e501a7a8160d5e7213cc252cd09c
Mahdyar Hasanpour <hi@mahdyar.me>
2021-08-11 18:22:43 +0430

    chore: remove redundant `getBrowser` function

--- script/functions.js
+++ script/functions.js
@@ -63,14 +63,3 @@ function sendToTelegram(url, title, telemark_code) {
     },
   });
 }
-function getBrowser() {
-  if (typeof chrome !== "undefined") {
-    if (typeof browser !== "undefined") {
-      return "Firefox";
-    } else {
-      return "Chrome";
-    }
-  } else {
-    return "Edge";
-  }
-}
